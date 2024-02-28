9a55890a072d45631c4680a87449cc70c539e570
bartoszsko43 <32441355-bartoszsko43@users.noreply.replit.com>
2024-02-28 14:22:09 +0000

    C2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello hddhdfhdfworld!");
+    System.out.println("Hello gdegedgeworld!");
   }
 
   // @Test
