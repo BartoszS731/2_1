fed7d6b916eccb7757d3379cb0889e8795a1a268
BartoszS731 <161606110+BartoszS731@users.noreply.github.com>
2024-03-13 13:27:12 +0000

    poprawka

--- src/main/zajecia/Hello.java
+++ src/main/zajecia/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
 public static void main(String[] args) {
-  System.out.println("Hello World!");
+  String nazwa="Hello World!";
+  for(int i=0;i<nazwa.lenght();i++)
+    {
+System.out.println(nazwa.charAt(i));   
+}                           
 }
 }
